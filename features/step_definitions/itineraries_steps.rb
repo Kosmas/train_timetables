25d524d6d68d7da09e20b9135261756575ddb77e
Kosmas Chatzimichalis <kosmas@mach7x.com>
2014-09-24 02:09:04 +0200

    added itineraries step definition file with pending steps

---
+++ features/step_definitions/itineraries_steps.rb
@@ -0,0 +1,48 @@
+Given(/^Western line trains from Emu Plains leave Paramatta for Town Hall at (\d+):(\d+), (\d+):(\d+), (\d+):(\d+), (\d+):(\d+), (\d+):(\d+), (\d+):(\d+), (\d+):(\d+), (\d+):(\d+)$/) do |arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15, arg16|
+    pending # express the regexp above with the code you wish you had
+end
+
+When(/^I want to travel from Parramata to Town Hall at (\d+):(\d+)$/) do |arg1, arg2|
+    pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should be told about the trains at: (\d+):(\d+), (\d+):(\d+), (\d+):(\d+), (\d+):(\d+)$/) do |arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8|
+    pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I want to go from Paramatta to Town Hall$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the next train is scheduled to leave at (\d+):(\d+) on the Western line$/) do |arg1, arg2|
+    pending # express the regexp above with the code you wish you had
+end
+
+When(/^I ask for my arrival time$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the estimated arrival time should be (\d+):(\d+)$/) do |arg1, arg2|
+    pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I want to go from Epping to Central$/) do
+    pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the next train is scheduled to leave at (\d+):(\d+) on the Northern line$/) do |arg1, arg2|
+    pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the next train is scheduled to leave at (\d+):(\d+) on the Newcastle line$/) do |arg1, arg2|
+    pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the estimated arrival time should be (\d+)\.(\d+)$/) do |arg1, arg2|
+    pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the next train is scheduled to leave at (\d+):(\d+) on the Epping line$/) do |arg1, arg2|
+    pending # express the regexp above with the code you wish you had
+end
+
